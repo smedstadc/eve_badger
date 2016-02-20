66aac84b4fffc83c9038b351dd23ddbebe2fbfc1
Corey Smedstad <smedstadc@gmail.com>
2016-02-20 17:17:18 -0500

    move version to lib/eve_badger/version.rb

--- lib/eve_badger.rb
+++ lib/eve_badger.rb
@@ -1,3 +1,4 @@
+require 'eve_badger/version'
 require 'eve_badger/eve_api'
 require 'eve_badger/endpoints'
 require 'eve_badger/response'
@@ -5,10 +6,6 @@ require 'eve_badger/cache'
 require 'eve_badger/throttle'
 
 module EveBadger
-  def self.version
-    @version ||= File.read(File.expand_path(File.join(File.dirname(__FILE__), '..', 'VERSION'))).chomp
-  end
-
   # provides the default user agent for EveAPI objects, it's a good idea to customize your user agent string to identify your application
   def self.default_user_agent
     "EveBadger-#{EveBadger.version}/Ruby-#{RUBY_VERSION}"
---
+++ lib/eve_badger/version.rb
@@ -0,0 +1,7 @@
+module EveBadger
+  VERSION = "0.1.4"
+
+  def self.version
+    VERSION
+  end
+end
